af3c30ebb878d4439955c81e7b3a7e142372a5d0
Seryozha02 <seryozha.asatryan02@gmail.com>
2021-12-21 17:26:31 +0400

    Brief3

---
+++ src/com/company/commons/constants/FileConstants.java
@@ -0,0 +1,8 @@
+package com.company.commons.constants;
+
+public class FileConstants {
+    public static final String IMAGE_RESOURCE_PATH = "./img/";
+    public static final String WHERE_PATH = "OneDrive";
+    public static final String FOLDER_NAME = "Documents/Brief1";
+    public static final String USER_CV_DATA = "data.js";
+}
